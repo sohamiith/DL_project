90747cc8e5feb3d0b44518aedabe1af0591e319e
sohamiith <sm21mtech14004@iith.ac.in>
2022-05-02 23:35:07 +0530

    final code

--- main.py
+++ main.py
@@ -26,14 +26,16 @@ def get_video_url():
 	audio = convertAudio(path)
 	text = generateText(audio,method)
 	summary = generateSummary(text,int(per)//100)
+	
+	# Json response for testing
 	# return jsonify(result = {
 	#  	"video_path" : path,
 	#  	"audio" : audio,
 	#  	"text": text,
 	#  	"summary": summary
 	# })
-	return render_template('result.html', url="url", text="text", summary="summary")
-	#return jsonify(result)
+	
+	return render_template('result.html', url=url, text=text, summary=summary)
 
 if __name__=="__main__":
 	app.run(debug=True)
